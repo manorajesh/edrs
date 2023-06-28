264d4e6539d58695554dc57a364260b0e8753710
manorajesh <mano@raje.sh>
2023-06-27 21:23:10 -0700

    Added proper `backspace` functionality

--- src/io.rs
+++ src/io.rs
@@ -147,7 +147,13 @@ fn process_key_code(key: KeyStroke, textbuf: &mut TextBuf) {
                 textbuf.cursor.0 -= 1;
                 textbuf.row_buffer[textbuf.cursor.1].remove(textbuf.cursor.0);
             } else if textbuf.cursor.1 > 0 {
-                textbuf.row_buffer.remove(textbuf.cursor.1);
+                if textbuf.row_buffer[textbuf.cursor.1].is_empty() {
+                    textbuf.row_buffer.remove(textbuf.cursor.1);
+                } else {
+                    textbuf.cursor.0 = textbuf.row_buffer[textbuf.cursor.1-1].len();
+                    let row = textbuf.row_buffer.remove(textbuf.cursor.1).into_iter();
+                    textbuf.row_buffer[textbuf.cursor.1-1].extend(row);
+                }
                 textbuf.cursor.1 -= 1;
                 textbuf.cursor.0 = textbuf.row_buffer[textbuf.cursor.1].len();
             }
@@ -251,6 +257,16 @@ fn process_key_code(key: KeyStroke, textbuf: &mut TextBuf) {
                 textbuf.cursor.1 = textbuf.row_buffer.len() - 1;
             }
 
+            textbuf.dirty = true;
+        }
+
+        KeyStroke(KeyCode::End, _) => {
+            textbuf.cursor.0 = textbuf.row_buffer[textbuf.cursor.1].len();
+            textbuf.dirty = true;
+        }
+
+        KeyStroke(KeyCode::Home, _) => {
+            textbuf.cursor.0 = 0;
             textbuf.dirty = true;
         }
 
